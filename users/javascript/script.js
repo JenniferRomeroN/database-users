ac33f8b7eeac4d25b1eaedae6a384c2137612111
jenni <jennifer_romero3@hotmail.com>
2024-10-02 16:24:21 -0700

    add validation event

--- users/javascript/script.js
+++ users/javascript/script.js
@@ -1,11 +1,35 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const toastTrigger = document.getElementById('liveToastBtn');
-    const toastLiveExample = document.getElementById('liveToast');
+//llamamos al boton
+const submitButton = document.getElementById('buttonSubmit');
+//llamamos al formulario
+const form = document.getElementById('user');
 
-    if (toastTrigger) {
-        const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample);
-        toastTrigger.addEventListener('click', () => {
-            toastBootstrap.show();
-        });
-    }
+//hacemos el evento con el boton
+submitButton.addEventListener("click", (event) => {
+  //no envia el formulario directamente, hasta que se haga la validacion
+  event.preventDefault();
+  //llamamos la funcion y si tiene todos los campos se enviara con form.submit
+  if(validarFormulario()){
+    form.submit();
+  }
 });
+
+function validarFormulario() {
+  // Obtener referencias a los elementos del formulario
+  const nombre = document.getElementById('name');
+  const apellido = document.getElementById('lastName');
+
+  // Verificar si los campos están vacíos
+  if (nombre.value.trim() === "") {
+      alert("Please, complete allfields!");
+      nombre.focus(); // Colocar el cursor en el campo de nombre
+      return false; // Detener el envío del formulario
+  }
+
+  if (apellido.value.trim() === "") {
+      alert("Please, complete allfields!");
+      apellido.focus();
+      return false;
+  }
+  // Si todos los campos están llenos, permitir el envío
+  return true;
+}
